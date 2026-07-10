2f82365c44f56f1f449281431e46da4447932db2
moonwoodgames <moonwoodgamesstudios@gmail.com>
2026-07-10 01:02:35 -0600

    Achievements
    I got the achievement system added and go the first 9 achievements in and working. Just 10 more to go (for now).

--- js/plugins/SRD_HighAFPicturesMZ.js
+++ js/plugins/SRD_HighAFPicturesMZ.js
@@ -4,7 +4,7 @@
 		// Place Pictures IDs in here you want to appear above HUD Maker.
 		// They must be separated by commas.
 
-		71, 72, 73, 74, 75, 76, 77, 78, 79, 80
+		71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 91
 	];
 
 	Spriteset_Base.prototype.createPictures = function () {
