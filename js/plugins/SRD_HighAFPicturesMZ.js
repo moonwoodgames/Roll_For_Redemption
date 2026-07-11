ef162a3a5ce5540f0ee4301dd4f926521e5b8645
moonwoodgames <moonwoodgamesstudios@gmail.com>
2026-07-11 14:45:02 -0600

    Finishing Quests
    Got the quests finally and fully finished. They add coins, they fade out faster and things just feel much better and snappier.

--- js/plugins/SRD_HighAFPicturesMZ.js
+++ js/plugins/SRD_HighAFPicturesMZ.js
@@ -4,7 +4,7 @@
 		// Place Pictures IDs in here you want to appear above HUD Maker.
 		// They must be separated by commas.
 
-		71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 91, 92, 94, 95, 96, 97, 98, 99
+		71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 91, 92, 94, 95, 96, 97, 98, 99, 100
 	];
 
 	Spriteset_Base.prototype.createPictures = function () {
